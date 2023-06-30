46e2e925ee2fc7c1a72b21e8b61b7ffc4278f59c
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-30 19:20:49 +0530

    Client side session

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import User from "@/components/User";
 
 export default async function Home() {
 	const session = await getServerSession(authOptions);
 	return (
 		<div>
 			<h1>Home</h1>
+			<h1>This is server side session</h1>
 			<code>{JSON.stringify(session)}</code>
+
+			<hr />
+			<p>Client side session</p>
+			<User />
 		</div>
 	);
 }
